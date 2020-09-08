104bb9e9b00230f93107f9afee1fff19fd683f7c
Mostafa Saeed <mostafa.saeed543@gmail.com>
2020-09-08 09:35:44 +0200

    [Init]

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page {{ test }}</h1>
+    <button v-on:click="idk">IDK</button>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    test: '',
+  }),
+  methods: {
+    idk() {
+      console.log('testing', process.env);
+      this.test = 123;
+    },
+  },
+};
+</script>
